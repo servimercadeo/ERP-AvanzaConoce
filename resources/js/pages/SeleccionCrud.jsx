d2f777ce464ce8ccc38e7ccb5d942b9101978b44
Camilo Marin <marin.jc2005@gmail.com>
2026-06-02 09:57:31 -0500

    Validacon tigo

--- resources/js/pages/SeleccionCrud.jsx
+++ resources/js/pages/SeleccionCrud.jsx
@@ -220,10 +220,6 @@ export default function SeleccionCrud() {
           </div>
         </div>
         <div style={S.row}>
-          <button style={S.btnOutline} onClick={reload}>
-            <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"><polyline points="1 4 1 10 7 10"/><polyline points="23 20 23 14 17 14"/><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"/></svg>
-            Actualizar
-          </button>
           <button style={S.btnPrimary} onClick={() => openModal('create')}>+ Nueva requisición</button>
         </div>
       </div>
